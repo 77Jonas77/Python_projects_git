f125cf9fc8868f50b00e0be513467a0898002216
s27523 <s27523@pjwstk.edu.pl>
2024-03-09 20:08:00 +0100

    Dodanie pierwszego programu z ksiazki Eric'a Matthes'a
    Instrukcje dla programisty z ksiazki Eric'a Matthes'a wersji 3

---
+++ #zmienna jako etykieta wartosci, a nie j.py
@@ -0,0 +1,7 @@
+#zmienna jako etykieta wartosci, a nie jako pojemnik na wartosc
+#why? -> zmienna wskazuje na  obiekt w pamieci. Jest odniesieniem -> nie przechowuje tej wartosci 
+messesage = "Witaj, swiecie Pythona"
+print(messesage)
+
+messesage = "Witaj, swiecie Pythona2"
+print(messesage)
